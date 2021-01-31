1712e41d100e6e72375db60158617c06154c5b7b
alexf4d <alex4d@live.com>
2021-01-31 16:26:50 -0600

    Added a Detected_Object class and some threading

--- snap.py
+++ snap.py
@@ -4,61 +4,98 @@ import sys, getopt
 import pafy
 import os.path
 from os import path
+import threading
+import time
+import datetime
 
-
-def Detect(frame):
+class Detected_Object():
+    def __init__(self, boxes, confidence, class_name):
+        self.bbox = boxes
+        self.confidence = confidence
+        self.class_name = class_name
+        
+        return
+    
+class Detect_Config():
     ##################### DETECTION CONFIG ##################################################
+    def __init__(self):
+        
+        self.CONF_THRESH = 0.5
+        self.NMS_THRESH = 0.5
 
-    CONF_THRESH, NMS_THRESH = 0.5, 0.5
+        # Load the network
+        self.net = cv2.dnn.readNet('yolov3.weights','yolov3.cfg')
+        self.classes = []
+        with open('coco.names', "r") as f:
+            self.classes = f.read().splitlines()
+        
+        return
 
-    # Load the network
-    net = cv2.dnn.readNetFromDarknet('yolov3.cfg', 'yolov3.weights')
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+def Detect(config, frame):
 
-    # Get the output layer from YOLO
-    layers = net.getLayerNames()
-    output_layers = [layers[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
+    # Get variables from configuration class
+    CONF_THRESH = config.CONF_THRESH
+    NMS_THRESH = config.NMS_THRESH
+    net = config.net
+    classes = config.classes
+ 
     # Read and convert the image to blob and perform forward pass to get the bounding boxes with their confidence scores
     height, width = frame.shape[:2]
 
-    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), swapRB=True, crop=False)
-    net.setInput(blob)
-    layer_outputs = net.forward(output_layers)
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0,0,0), swapRB=True, crop=False)
     
-    class_ids, confidences, b_boxes = [], [], []
+    net.setInput(blob)
+
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    layer_outputs = net.forward(output_layers_names)
+    
+    boxes = []
+    confidences = []
+    class_ids = []
+
     for output in layer_outputs:
         for detection in output:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
 
-            if confidence > CONF_THRESH:
+            if (confidence > CONF_THRESH):
+                
                 center_x, center_y, w, h = (detection[0:4] * np.array([width, height, width, height])).astype('int')
 
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
 
-                b_boxes.append([x, y, int(w), int(h)])
-                confidences.append(float(confidence))
+                boxes.append([x, y, int(w), int(h)])
+                confidences.append((float(confidence)))
                 class_ids.append(int(class_id))
-    try:
-        indices = cv2.dnn.NMSBoxes(b_boxes, confidences, CONF_THRESH, NMS_THRESH).flatten().tolist()
-        
-        NMSBoxes = []
-        for index in indices:
-            NMSBoxes.append(b_boxes[index])
-
-    except:
+    
+    
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, CONF_THRESH, NMS_THRESH)
+    
+    if len(indices)>0:
+        detected_objects = []
+        for index in indices.flatten():
+            detected_object = None
+            detected_object = Detected_Object(boxes[index],confidences[index],classes[class_ids[index]])
+            detected_objects.append(detected_object)
+            
+            print(detected_object.bbox)
+            print(detected_object.confidence)
+            print(detected_object.class_name)
+            
+        return(detected_objects)
+    else:
         return
 
-    return(NMSBoxes)
-
-def Take_a_Picture(image_id, video, original_frame):
+def Take_a_Picture(image_location, original_frame):
     frame = original_frame
+    
+    # using now() to get current time  
+    current_time = datetime.datetime.now()
+
     for x in range(1):
-        image_name = image_id+"-"+str(x+1)+".jpg"
+        image_name = image_location+str(current_time)+".jpg"
         cv2.imwrite(image_name, frame)
     return(image_name)
 
@@ -117,6 +154,28 @@ def Config_File(input_file):
     
     return(video)    
 
+def Helper(config, frame, image_location):
+    detected_objects = []
+    detected_objects = Detect(config, frame)
+
+    classes = config.classes
+    if detected_objects:
+        annotation=True
+        if annotation:
+            colors = []
+            colors = np.random.uniform(0, 255, size=(len(classes), 3))
+            
+            for i, detected_object in enumerate(detected_objects,1):
+                x, y, w, h = detected_object.bbox
+                print (x,y,w,h)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), colors[i], 2)
+                cv2.putText(frame, str(round(detected_object.confidence, 2)) , (x + 5, y + 20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, colors[i], 2)
+                
+        
+        print("Taking a picture...")
+        image_name = Take_a_Picture(image_location, frame)
+        print(image_name)
+
 def main(argv):
     
     #Read arguments
@@ -154,42 +213,30 @@ def main(argv):
     elif (input_file):
         video = Config_File(input_file)
 
+    
+    
+    # Configure Detection
+    config = Detect_Config()
 
     # Detect object and Snap
-    detect_counter = 31
+    detect_counter = 61
     NMSBoxes = None
-    photo_id = 1
+
+    cv2.namedWindow("Snap", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Snap", 1280, 720)
 
     while True:
         ok, frame = video.read()
         
         NMSBoxes = None
-       
-        if (detect_counter > 30):          
-            NMSBoxes = Detect(frame)
+
+        if (detect_counter > 60):
+            x = threading.Thread(target=Helper, args=(config, frame, image_location))
+            print("starting a Helper thread")
+            x.start()
+            x.join()
             detect_counter = 0
-
-            if NMSBoxes:
-                annotation=False
-                if annotation:
-                    with open('coco.names', "r") as f:
-                        classes = [line.strip() for line in f.readlines()]
-                    colors = np.random.uniform(0, 255, size=(len(classes), 3))
-
-                    for index in NMSBoxes:
-                        x, y, w, h = NMSBoxes[index]
-                        cv2.rectangle(frame, (x, y), (x + w, y + h), colors[index], 2)
-                        cv2.putText(frame, classes[class_ids[index]], (x + 5, y + 20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, colors[index], 2)
-                
-                print("Taking a picture...")
-                image_name = Take_a_Picture(image_location+str(photo_id), video, frame)
-                photo_id=photo_id+1
-                print(image_name)
-
-                snappy_messages= ["Snapping a Photo!", "Cheese!", "Taking the Picture.", "Shake your tail feathers!"]
-                cv2.rectangle(frame, (0, 0), (0 + 1280, 0 + 720), 0, 2)
-                cv2.putText(frame, snappy_messages[np.random.randint(0,len(snappy_messages))], (25, 25), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, 100, 2)
-        
+            
         detect_counter = detect_counter + 1
         
 
@@ -203,8 +250,7 @@ def main(argv):
         """
 
 
-        cv2.namedWindow("Snap", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("Snap", 1280, 720)
+        time.sleep(int(1/30))
         cv2.imshow("Snap", frame)
         
         k = cv2.waitKey(1) & 0xff
